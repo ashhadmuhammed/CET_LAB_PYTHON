748bee697a519f55dd5ad23286783413f1b3ff15
Ashhad Muhammed Kader <93571194+ashhadmuhammed@users.noreply.github.com>
2021-11-24 11:41:07 +0530

    Add files via upload

---
+++ exc 2(circle)/circle.py
@@ -0,0 +1,4 @@
+import math
+r=int(input("Enter the radius of the Circle:"))
+print("Area of the circle with ",r," unit radius:",math.pi*r**2)
+print("Circumference of the circle with ",r," unit radius:",math.pi*r*2)
